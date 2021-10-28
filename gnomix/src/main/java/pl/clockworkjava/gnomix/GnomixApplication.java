0c7566ac1a432bf74b19ab58e66f923c79f393ab
kt1313 <tomaklim@wp.pl>
2021-10-28 12:35:56 +0200

    Initial commit

---
+++ gnomix/src/main/java/pl/clockworkjava/gnomix/GnomixApplication.java
@@ -0,0 +1,13 @@
+package pl.clockworkjava.gnomix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnomixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnomixApplication.class, args);
+	}
+
+}
