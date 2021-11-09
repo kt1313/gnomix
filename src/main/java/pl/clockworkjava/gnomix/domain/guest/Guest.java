f09bb39474902acb0e43a795694acd516d628c01
kt1313 <tomaklim@wp.pl>
2021-11-09 15:38:34 +0100

    14. 6_1 ID --- dodawanie id do Guesta, w dziwny sposob

--- src/main/java/pl/clockworkjava/gnomix/domain/guest/Guest.java
+++ src/main/java/pl/clockworkjava/gnomix/domain/guest/Guest.java
@@ -3,15 +3,28 @@ package pl.clockworkjava.gnomix.domain.guest;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 
 @Data
 public class Guest {
 
+    private final long id;
     private final String firstName;
     private final String lastName;
     private final LocalDate birthDate;
     private  final  Gender gender;
+
+
+    public Guest( String firstName,
+                 String lastName, LocalDate birthDate,
+                 Gender gender) {
+        this.id = UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.gender = gender;
+    }
 
     @Override
     public String toString() {
