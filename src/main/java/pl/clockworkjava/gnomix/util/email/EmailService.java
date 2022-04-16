b9f5f20cddc0e71277d73ed522231e21fd0725f8
kt1313 <tomaklim@wp.pl>
2022-04-16 11:07:22 +0200

    49.11. - Parametryzacja Konfiguracji do EmailSender - wstawienie wartosci parametrow Maila do pproperties - wciaz trzeba dopisac w propertiesach password do maila - jeszcze nie zrobione

--- src/main/java/pl/clockworkjava/gnomix/util/email/EmailService.java
+++ src/main/java/pl/clockworkjava/gnomix/util/email/EmailService.java
@@ -1,6 +1,7 @@
 package pl.clockworkjava.gnomix.util.email;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,17 +11,26 @@ public class EmailService {
 
     private final JavaMailSender sender;
 
+    @Value("@{gnomix.protocol}")
+    private String protocol;
+    @Value("@{gnomix.domain}")
+    private String domain;
+    @Value("@{gnomix.port}")
+    private String port;
+    private String confirmationEndpoint="reservation/confirm";
+
     @Autowired
     public EmailService(JavaMailSender sender){
         this.sender=sender;
     }
     public void sendConfirmationEmail(String email, long reservationID) {
-        String text="http://localhost:8080/reservation/confirm/"+reservationID;
+
+        String endpoint=String.format("%s://%s:%s/%s/%d", protocol, domain, port, confirmationEndpoint, reservationID);
         SimpleMailMessage mail=new SimpleMailMessage();
         mail.setTo(email);
         mail.setFrom("systemgnomix@gmail.com");
         mail.setSubject("Potweirdzenie rezerwacji");
-        mail.setText("Dziękujemy za rezerwację, by ją potwierdzić, kliknij w link "+text);
+        mail.setText("Dziękujemy za rezerwację, by ją potwierdzić, kliknij w link "+endpoint);
         this.sender.send(mail);
     }
 }
