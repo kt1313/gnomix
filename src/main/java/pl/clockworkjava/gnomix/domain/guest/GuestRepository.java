b2248db7756e1674a68aa908410aab9f4970edda
kt1313 <tomaklim@wp.pl>
2022-07-14 05:01:46 +0200

    72.15.02 - SpringData i Zarzadzanie Baza Danych w Projekcie - QSpring Data – zapytania wieloparametrowe i łączenie warunków to co poprzednio, tylko b skomplikowane testy jednostkowe , upaszczanie poprzez tworzenie zapytan SQL poprzez SpringData

--- src/main/java/pl/clockworkjava/gnomix/domain/guest/GuestRepository.java
+++ src/main/java/pl/clockworkjava/gnomix/domain/guest/GuestRepository.java
@@ -4,7 +4,17 @@ package pl.clockworkjava.gnomix.domain.guest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 @Repository
 public interface GuestRepository extends JpaRepository<Guest,Long> {
+
+    Optional<Guest> findTop1ByCustomerIdAndFirstNameAndLastNameAndBirthDate(
+            String customerId,
+            String firstName,
+            String lastName,
+            LocalDate birthDate
+    );
 
 }
---
+++ src/test/java/pl/clockworkjava/gnomix/domain/guest/GuestRepositoryTest.java
@@ -0,0 +1,50 @@
+package pl.clockworkjava.gnomix.domain.guest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+public class GuestRepositoryTest {
+
+    @Autowired
+    private GuestRepository guestRepository;
+
+    @Test
+    public void getCustomerById() {
+
+        LocalDate n = LocalDate.now();
+
+        Guest g1 = new Guest("Pawel", "Cwik", n, Gender.MALE, true);
+        g1.setCustomerId("A123");
+
+        Guest g2 = new Guest("Pawel", "Cwik", n, Gender.FEMALE, true);
+        g2.setCustomerId("Awww");
+
+        this.guestRepository.save(g1);
+        this.guestRepository.save(g2);
+
+        Optional<Guest> result=this.guestRepository.findTop1ByCustomerIdAndFirstNameAndLastNameAndBirthDate("" +
+                "A123",
+                "Pawel",
+                "Cwik",
+                n
+        );
+
+        assertTrue(result.isPresent());
+
+         result=this.guestRepository.findTop1ByCustomerIdAndFirstNameAndLastNameAndBirthDate("" +
+                        "A333",
+                "Pawel",
+                "Cwik",
+                n
+        );
+
+        assertTrue(result.isEmpty());
+    }
+}
