1b05407c723dad58c9352ae10f58d35a68d7db5b
kt1313 <tomaklim@wp.pl>
2022-07-18 06:27:56 +0200

    81.16.3 - Sprin Security - Ustawienie wolnego dostępu do wybranych zasobów - konfiguracvja cd,.

--- src/main/java/pl/clockworkjava/gnomix/security/ApplicationSecurityConfig.java
+++ src/main/java/pl/clockworkjava/gnomix/security/ApplicationSecurityConfig.java
@@ -14,6 +14,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter
     @Override
     protected  void configure (HttpSecurity http) throws Exception{
         http.authorizeRequests()
+                .antMatchers("/v3/ap-docs/*")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
