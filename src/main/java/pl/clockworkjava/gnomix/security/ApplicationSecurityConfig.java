c10162ebce4320435b3e6f7037815056b7e20fc6
kt1313 <tomaklim@wp.pl>
2022-07-18 18:07:28 +0200

    82.16.4 - Sprin Security - Pierwsze podejscie do przechowywania uzytkownikow i szyfrowania hasel

--- src/main/java/pl/clockworkjava/gnomix/security/ApplicationSecurityConfig.java
+++ src/main/java/pl/clockworkjava/gnomix/security/ApplicationSecurityConfig.java
@@ -1,24 +1,48 @@
 package pl.clockworkjava.gnomix.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter
 {
+
+    private PasswordEncoder encoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder encoder){
+        this.encoder=encoder;
+    }
     @Override
     protected  void configure (HttpSecurity http) throws Exception{
         http.authorizeRequests()
-                .antMatchers("/v3/ap-docs/*")
+                .antMatchers("/v3/api-docs/*")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("pawelcwik")
+                .password(this.encoder.encode("pawelcwik"))
+                .roles("OWNER").build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
