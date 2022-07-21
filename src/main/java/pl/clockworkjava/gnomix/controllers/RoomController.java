63f21b8360e33a15a1e5237bc2dba96cc3c9697c
kt1313 <tomaklim@wp.pl>
2022-07-21 06:47:20 +0200

    85.16.7 - Sprin Security - CSRF _wprowadzanie zabezpieczen i ról za pomoca CSRFa (ogolnie, z tokenem !!!! do aplikacji obslugiwanych poprzez http)

--- src/main/java/pl/clockworkjava/gnomix/controllers/RoomController.java
+++ src/main/java/pl/clockworkjava/gnomix/controllers/RoomController.java
@@ -2,6 +2,7 @@ package pl.clockworkjava.gnomix.controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -50,9 +51,9 @@ public class RoomController {
     }
 
     @GetMapping("/delete/{id}")
+    @PreAuthorize("hasRole('ROLE_MANAGER')")
     public String removeRoomById(@PathVariable("id") long id) {
         this.roomService.removeRoomById(id);
-
         return "redirect:/rooms";
     }
 
--- src/main/java/pl/clockworkjava/gnomix/security/ApplicationSecurityConfig.java
+++ src/main/java/pl/clockworkjava/gnomix/security/ApplicationSecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -31,7 +32,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter
     @Override
     protected  void configure (HttpSecurity http) throws Exception{
         http
-                .csrf().disable()
+//                .csrf().disable()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
                 .authorizeRequests()
                 .antMatchers("/v3/api-docs/*")
                 .permitAll()
