24614cfea6340c9d048fe1f526568e0defe87089
kt1313 <tomaklim@wp.pl>
2022-07-22 06:33:42 +0200

    87.16.9 - Sprin Security - Wlasna Strona Logowania

--- src/main/java/pl/clockworkjava/gnomix/controllers/HomeController.java
+++ src/main/java/pl/clockworkjava/gnomix/controllers/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home(){
         return "home";
     }
+
+    @GetMapping("/login")
+    public String getLogin(){
+        return "login";
+    }
 }
--- src/main/java/pl/clockworkjava/gnomix/security/ApplicationSecurityConfig.java
+++ src/main/java/pl/clockworkjava/gnomix/security/ApplicationSecurityConfig.java
@@ -46,7 +46,11 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/rooms", true)
+                .and()
+                .rememberMe();
     }
 
     @Override
