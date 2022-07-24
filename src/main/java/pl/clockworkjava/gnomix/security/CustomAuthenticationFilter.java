a3c0eed63c8d634411afd22c9a29e9f8649b0ad7
kt1313 <tomaklim@wp.pl>
2022-07-24 03:50:55 +0200

    93.16.15 - Sprin Security - Zwracanie tokena w ciele odpowiedzi - wrzuca tokena do response

--- src/main/java/pl/clockworkjava/gnomix/security/CustomAuthenticationFilter.java
+++ src/main/java/pl/clockworkjava/gnomix/security/CustomAuthenticationFilter.java
@@ -19,7 +19,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -69,5 +73,10 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                 .sign(algorithm);
         response.addHeader("access_token", accessToken);
         response.addHeader("refresh_token", refreshToken);
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("access_token", accessToken);
+        tokens.put("refresh_token", refreshToken);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
     }
 }
