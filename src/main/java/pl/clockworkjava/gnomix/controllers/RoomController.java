708a7001f718bc6cefce87707ad46df23660a85d
kt1313 <69506082+kt1313@users.noreply.github.com>
2022-10-26 04:24:02 +0200

    100.17.7 - Spring Security - CSRF wytlumaczenie

--- src/main/java/pl/clockworkjava/gnomix/controllers/RoomController.java
+++ src/main/java/pl/clockworkjava/gnomix/controllers/RoomController.java
@@ -1,6 +1,7 @@
 package pl.clockworkjava.gnomix.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,6 +44,7 @@ public class RoomController {
     }
 
     @GetMapping("/delete/{id}")
+    @PreAuthorize("hasRole('ROLE_MANAGER')")
     public String removeRoom(@PathVariable long id) {
 
         this.roomService.removeById(id);
--- src/main/java/pl/clockworkjava/gnomix/security/ApplicationSecurityConfig.java
+++ src/main/java/pl/clockworkjava/gnomix/security/ApplicationSecurityConfig.java
@@ -7,11 +7,13 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -24,7 +26,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
                 .authorizeRequests()
                 .antMatchers("/v3/api-docs/*")
                 .permitAll()
